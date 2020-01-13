30dda1654ac058788fdd31cf9d5ef453fb5813b7
San4es0413 <alexkalinin13@gmail.com>
2020-01-13 23:00:34 +0300

    Add template "home" for main Redacting main page

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Tutorial
 from django.http import HttpResponse
-
-# Create your views here.
 
 
 def homepage(request):
-    return HttpResponse("Wow this is an <strong>awesome</strong> tutorial")
+    return render(request=request,
+                  template_name="main/home.html",
+                  context={"tutorials": Tutorial.objects.all})
