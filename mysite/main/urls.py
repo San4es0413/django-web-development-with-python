c62fdf240dacd0a4527736e479e5575afdeb0b38
San4es0413 <alexkalinin13@gmail.com>
2020-01-14 02:21:19 +0300

    Create Register form

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -19,4 +19,5 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
+    path("register/", views.register, name="register"),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tutorial
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, logout, authenticate
 
 
 def homepage(request):
     return render(request=request,
                   template_name="main/home.html",
                   context={"tutorials": Tutorial.objects.all})
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("main:homepage")
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+    form = UserCreationForm
+    return render(request,
+                  "main/register.html",
+                  context={"form": form})
