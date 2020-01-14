6a0d8635d6edc21d9d3918edc3d678db3a08c19c
San4es0413 <alexkalinin13@gmail.com>
2020-01-14 15:39:51 +0300

    Add Login/Logout functional Create NewRegisterForm

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
 from .models import Tutorial
-from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
+from .forms import NewUserForm
 
 
 def homepage(request):
@@ -14,7 +14,7 @@ def homepage(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = NewUserForm(request.POST)
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
@@ -26,7 +26,33 @@ def register(request):
             for msg in form.error_messages:
                 messages.error(request, f"{msg}: form.error_messages[msg]")
 
-    form = UserCreationForm
+    form = NewUserForm
     return render(request,
                   "main/register.html",
                   context={"form": form})
+
+
+def logout_request(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main:homepage")
+
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password")
+
+    form = AuthenticationForm()
+    return render(request,
+                  "main/login.html",
+                  {"form": form})
