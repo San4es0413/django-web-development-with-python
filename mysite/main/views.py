d38cd5e7e6cf850c0a01aa320ba0823122b95fc2
San4es0413 <alexkalinin13@gmail.com>
2020-01-14 12:57:40 +0300

    Add toast messages Restructuring templates

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,6 +3,7 @@ from .models import Tutorial
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
 def homepage(request):
@@ -16,11 +17,14 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account Created: {username}")
             login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print(form.error_messages[msg])
+                messages.error(request, f"{msg}: form.error_messages[msg]")
 
     form = UserCreationForm
     return render(request,
