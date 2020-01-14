34150293193b97150f2f48aedebb7bc954af14d5
San4es0413 <alexkalinin13@gmail.com>
2020-01-14 23:52:38 +0300

    Create Category and Series for Tutorials Fill DB with data

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,15 +1,38 @@
 from django.shortcuts import render, redirect
-from .models import Tutorial
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 from .forms import NewUserForm
+from django.http import HttpResponse
+
+
+def single_slug(request, single_slug):
+    categories = [c.category_slug for c in TutorialCategory.objects.all()]
+    if single_slug in categories:
+        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+
+        series_urls = {}
+        for m in matching_series.all():
+            part_one = Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series)\
+                .earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
+
+        return render(request,
+                      "main/category.html",
+                      {"part_ones": series_urls})
+
+    tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
+    if single_slug in tutorials:
+        return HttpResponse(f"{single_slug} is a tutorial!!!")
+
+    return HttpResponse(f"{single_slug} does not correspond to anything.")
 
 
 def homepage(request):
     return render(request=request,
-                  template_name="main/home.html",
-                  context={"tutorials": Tutorial.objects.all})
+                  template_name="main/categories.html",
+                  context={"categories": TutorialCategory.objects.all})
 
 
 def register(request):
